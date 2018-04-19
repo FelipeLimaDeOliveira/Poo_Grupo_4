586aa305778152d78bbd54c4fb41a2a7a28e561c
Felipe <FelipeLimaDeOliveira>
2018-04-18 23:55:06 -0300

    Criado e iniciado classe Clientes

---
+++ Poo_Grupo_4/src/Cadastros/Clientes.java
@@ -0,0 +1,19 @@
+package Cadastros;
+public class Clientes {
+    String nome;
+    String email;
+    String endereco;
+    String cpf;
+    String rg;
+    String telefone;
+
+public void setClientes(String nome, String email, String endereco, String cpf, String rg, String telefone){
+this.setNome(nome);
+this.setEmail(email);
+this.setEndereco(endereco);
+this.setCpf(cpf);
+this.setRg(rg);
+this.setTelefone(telefone);
+}
+
+}
